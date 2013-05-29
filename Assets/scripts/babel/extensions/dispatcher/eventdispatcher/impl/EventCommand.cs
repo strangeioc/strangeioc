8a031d49f3fa343eb805b2418f9f43f6fd33e395
Marc Tanenbaum <marc@thirdmotion.com>
2013-05-29 15:22:08 -0700

    Ensure context dispatcher gets injected into commands. Ensure addListener returns existing bindings in all cases.

--- Assets/scripts/babel/extensions/dispatcher/eventdispatcher/impl/EventCommand.cs
+++ Assets/scripts/babel/extensions/dispatcher/eventdispatcher/impl/EventCommand.cs
@@ -4,6 +4,7 @@
  */
 
 using System;
+using babel.extensions.context.api;
 using babel.extensions.dispatcher.eventdispatcher.api;
 using babel.extensions.command.api;
 using babel.extensions.command.impl;
@@ -12,7 +13,7 @@ namespace babel.extensions.dispatcher.eventdispatcher.impl
 {
 	public class EventCommand : babel.extensions.command.impl.Command
 	{
-		[Inject]
+		[Inject(ContextKeys.CONTEXT_DISPATCHER)]
 		public IEventDispatcher dispatcher{ get; set;}
 
 		[Inject]
