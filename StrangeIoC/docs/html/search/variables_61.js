7200eb52f70710e40be1134811f4d02014ea9b5f
Marc Tanenbaum <marc@thirdmotion.com>
2013-06-25 09:03:09 -0700

    Updating documentation for v0.51

--- StrangeIoC/docs/html/search/variables_61.js
+++ StrangeIoC/docs/html/search/variables_61.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['activecommands',['activeCommands',['../classstrange_1_1extensions_1_1command_1_1impl_1_1_command_binder.html#a8b6b770089f105b017000c9957aa7125',1,'strange::extensions::command::impl::CommandBinder']]],
+  ['activesequences',['activeSequences',['../classstrange_1_1extensions_1_1command_1_1impl_1_1_command_binder.html#ada066db5cf3c3014735a7874260ed4b1',1,'strange::extensions::command::impl::CommandBinder']]],
   ['autostartup',['autoStartup',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_context.html#a3386ea2967ba720218b89eaefac77d56',1,'strange::extensions::context::impl::Context']]]
 ];
