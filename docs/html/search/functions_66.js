5b803ad449eb373eed62828da07a783a38669273
Marc Tanenbaum <marc@thirdmotion.com>
2013-06-25 09:05:54 -0700

    Updating documentation to v0.51 for github pages

---
+++ docs/html/search/functions_66.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fail',['Fail',['../interfacestrange_1_1extensions_1_1command_1_1api_1_1_i_command.html#a1ad585b6fcc3e4dad5823100a62d9053',1,'strange.extensions.command.api.ICommand.Fail()'],['../classstrange_1_1extensions_1_1command_1_1impl_1_1_command.html#aa91c0b75c21945fdf87105d806d3ba79',1,'strange.extensions.command.impl.Command.Fail()'],['../classstrange_1_1extensions_1_1sequencer_1_1impl_1_1_sequence_command.html#a338497a5f3812ed4b115cf222f372b9e',1,'strange.extensions.sequencer.impl.SequenceCommand.Fail()']]]
+];
