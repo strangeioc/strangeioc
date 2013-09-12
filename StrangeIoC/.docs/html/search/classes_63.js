3e4d7842e4b1628212633eea73c16764458ff33b
Marc Tanenbaum <marc@thirdmotion.com>
2013-09-12 11:18:09 -0700

    Regenerated doxygen with new info regarding CrossContextBridge

--- StrangeIoC/.docs/html/search/classes_63.js
+++ StrangeIoC/.docs/html/search/classes_63.js
@@ -9,6 +9,7 @@ var searchData=
   ['contextexception',['ContextException',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_context_exception.html',1,'strange::extensions::context::impl']]],
   ['contextview',['ContextView',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_context_view.html',1,'strange::extensions::context::impl']]],
   ['crosscontext',['CrossContext',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_cross_context.html',1,'strange::extensions::context::impl']]],
-  ['crosscontextinjectionbinder',['CrossContextInjectionBinder',['../classstrange_1_1extensions_1_1injector_1_1impl_1_1_cross_context_injection_binder.html',1,'strange::extensions::injector::impl']]],
-  ['crosscontextinjectionbinder',['CrossContextInjectionBinder',['../classstrange_1_1extensions_1_1injector_1_1api_1_1_cross_context_injection_binder.html',1,'strange::extensions::injector::api']]]
+  ['crosscontextbridge',['CrossContextBridge',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_cross_context_bridge.html',1,'strange::extensions::context::impl']]],
+  ['crosscontextinjectionbinder',['CrossContextInjectionBinder',['../classstrange_1_1extensions_1_1injector_1_1api_1_1_cross_context_injection_binder.html',1,'strange::extensions::injector::api']]],
+  ['crosscontextinjectionbinder',['CrossContextInjectionBinder',['../classstrange_1_1extensions_1_1injector_1_1impl_1_1_cross_context_injection_binder.html',1,'strange::extensions::injector::impl']]]
 ];
---
+++ StrangeIoC/.docs/html/search/variables_65.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['eventsinprogress',['eventsInProgress',['../classstrange_1_1extensions_1_1context_1_1impl_1_1_cross_context_bridge.html#a8ccc8f8fa78e4bd6fd2db7741f16feaf',1,'strange::extensions::context::impl::CrossContextBridge']]]
+];
