4e44e7f496c4eeb730ae4e8fcb4434a23e5f3a35
Marc Tanenbaum <tanenbaum@sfmultimedia.net>
2014-02-04 22:03:04 -0800

    Hotfix for ContextView. Keep OnDestroy from removing Context if there's no firstContext

--- StrangeIoC/scripts/strange/extensions/context/impl/ContextView.cs
+++ StrangeIoC/scripts/strange/extensions/context/impl/ContextView.cs
@@ -52,7 +52,7 @@ namespace strange.extensions.context.impl
 		/// </summary>
 		protected virtual void OnDestroy()
 		{
-			if (context != null)
+			if (context != null && Context.firstContext != null)
 				Context.firstContext.RemoveContext(context);
 		}
 
