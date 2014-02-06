8c969b47a6d2b57631969f981c64e24c0c35a25b
Marc Tanenbaum <tanenbaum@sfmultimedia.net>
2014-02-05 16:11:18 -0800

    Integrated an actual Unity3D project into StrangeIoC so we can do better testing in-situ

---
+++ StrangeIoC/examples/Assets/scripts/justtests/view/TesterMediator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using strange.extensions.mediation.impl;
+
+namespace strange.examples.justtests.view
+{
+	public class TesterMediator : Mediator
+	{
+		[Inject]
+		public ITesterView view{ get; set; }
+
+
+		override public void OnRegister()
+		{
+			view.Init ();
+		}
+	}
+}
+
