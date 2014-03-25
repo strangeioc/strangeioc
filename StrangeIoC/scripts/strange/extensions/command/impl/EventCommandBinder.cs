7ce8b661481e1942461e5c5cb7a97eb196b607a1
Will Corwin <wcorwin@gmail.com>
2014-03-24 22:07:38 -0700

    fix bug in event command binder preventing graceful unbinding of ICommand and IEvent

--- StrangeIoC/scripts/strange/extensions/command/impl/EventCommandBinder.cs
+++ StrangeIoC/scripts/strange/extensions/command/impl/EventCommandBinder.cs
@@ -32,7 +32,6 @@ namespace strange.extensions.command.impl
 		public EventCommandBinder ()
 		{
 		}
-
 		/// 
 		override protected ICommand createCommand(object cmd, object data)
 		{
@@ -42,25 +41,36 @@ namespace strange.extensions.command.impl
 				injectionBinder.Bind<IEvent>().ToValue(data).ToInject(false);
 			}
 
-			ICommand command = injectionBinder.GetInstance<ICommand> () as ICommand;
-			if (command == null)
+			ICommand command = injectionBinder.GetInstance<ICommand>() as ICommand;
+			try
 			{
-				string msg = "A Command ";
+				if (command == null)
+				{
+					string msg = "A Command ";
+					if (data is IEvent)
+					{
+						IEvent evt = (IEvent) data;
+						msg += "tied to event " + evt.type;
+					}
+					msg += " could not be instantiated.\nThis might be caused by a null pointer during instantiation or failing to override Execute (generally you shouldn't have constructor code in Commands).";
+					throw new CommandException(msg, CommandExceptionType.BAD_CONSTRUCTOR);
+				}
+
+				command.data = data;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+			finally
+			{
 				if (data is IEvent)
 				{
-					IEvent evt = (IEvent)data;
-					msg += "tied to event " + evt.type;
+					injectionBinder.Unbind<IEvent>();
 				}
-				msg += " could not be instantiated.\nThis might be caused by a null pointer during instantiation or failing to override Execute (generally you shouldn't have constructor code in Commands).";
-				throw new CommandException(msg, CommandExceptionType.BAD_CONSTRUCTOR);
+				injectionBinder.Unbind<ICommand>();
 			}
-
-			command.data = data;
-			if (data is IEvent)
-			{
-				injectionBinder.Unbind<IEvent>();
-			}
-			injectionBinder.Unbind<ICommand> ();
+			
 			return command;
 		}
 
