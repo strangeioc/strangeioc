dd33a7a8d7550502e47a2e5c5beed667e949bd87
Will Corwin <wcorwin@gmail.com>
2015-02-03 01:19:54 -0800

    ListensTo cleanup. Refactor handling and cleanup signals during OnRemove

--- StrangeIoC/scripts/strange/extensions/mediation/SignalMediationBinder.cs
+++ StrangeIoC/scripts/strange/extensions/mediation/SignalMediationBinder.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
 using strange.extensions.mediation.api;
 using strange.extensions.mediation.impl;
@@ -19,32 +18,56 @@ namespace strange.extensions.mediation
             MonoBehaviour mediator = base.createMediator(mono, mediatorType);
             if (mediator is IMediator)
             {
-                IReflectedClass reflectedClass = reflectionBinder.Get(mediatorType);
-
-                //Let's GetInstance some Signals and add listeners
-
-                foreach (var pair in reflectedClass.attrMethods)
-                {
-                    if (pair.Value is ListensTo)
-                    {
-                        ListensTo attr = (ListensTo) pair.Value;
-                        ISignal signal = (ISignal) injectionBinder.GetInstance(attr.type);
-
-                        AssignDelegate(mediator, signal, pair.Key);
-                    }
-                }
-
-
+                HandleDelegates(mediator, mediatorType, true);
             }
             return mediator;
+        }
+
+        private void HandleDelegates(MonoBehaviour mono, Type mediatorType, bool toAdd)
+        {
+            IReflectedClass reflectedClass = reflectionBinder.Get(mediatorType);
+
+            //Let's GetInstance some Signals and add listeners
+
+            foreach (var pair in reflectedClass.attrMethods)
+            {
+                if (pair.Value is ListensTo)
+                {
+                    ListensTo attr = (ListensTo)pair.Value;
+                    ISignal signal = (ISignal)injectionBinder.GetInstance(attr.type);
+                    if (toAdd)
+                        AssignDelegate(mono, signal, pair.Key);
+                    else
+                        RemoveDelegate(mono, signal, pair.Key);
+                }
+            }
+        }
+
+        private void RemoveDelegate(MonoBehaviour mediator, ISignal signal, MethodInfo method)
+        {
+            if (signal.GetType().BaseType.IsGenericType) //e.g. Signal<T>, Signal<T,U> etc.
+            {
+                Delegate toRemove = Delegate.CreateDelegate(signal.listener.GetType(), mediator, method);
+                signal.listener = Delegate.Remove(signal.listener,toRemove);
+            }
+            else
+            {
+                ((Signal)signal).RemoveListener((Action)Delegate.CreateDelegate(typeof(Action), mediator, method)); //Assign and cast explicitly for Type == Signal case
+            }
+
         }
 
         private void AssignDelegate(MonoBehaviour mediator, ISignal signal, MethodInfo method)
         {
             if (signal.GetType().BaseType.IsGenericType)
+            {
                 signal.listener = Delegate.CreateDelegate(signal.listener.GetType(), mediator, method); //e.g. Signal<T>, Signal<T,U> etc.
+            }
             else
+            {
                 ((Signal)signal).AddListener((Action)Delegate.CreateDelegate(typeof(Action), mediator, method)); //Assign and cast explicitly for Type == Signal case
+            }
+
         }
 
 
@@ -52,6 +75,8 @@ namespace strange.extensions.mediation
         protected override void removeMediator(IMediator mediator)
         {
             //Unbind signals to methods
+            HandleDelegates((MonoBehaviour)mediator, mediator.GetType(), false);
+
             base.removeMediator(mediator);
         }
     }
